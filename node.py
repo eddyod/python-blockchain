4cedfd03dd77c129672d2fe2e0298d132c1657aa
Eddy O'Donnell <eddyod@yahoo.com>
2019-05-08 07:03:10 -0700

    Added static and class methods

--- node.py
+++ node.py
@@ -24,7 +24,7 @@ class Node:
     def print_blockchain_elements(self):
         """ Output all blocks of the blockchain. """
         # Output the blockchain list to the console
-        for block in self.blockchain.chain:
+        for block in self.blockchain.get_chain():
             print('Outputting Block')
             print(block)
         else:
@@ -36,7 +36,6 @@ class Node:
         # A while loop for the user input interface
         # It's a loop that exits once waiting_for_input becomes False or when break is called
         while waiting_for_input:
-            verifier = Verification()
             print('Please choose')
             print('a: Add a new transaction value')
             print('m: Mine a new block')
@@ -52,13 +51,13 @@ class Node:
                     print('Added transaction!')
                 else:
                     print('Transaction failed!')
-                print(self.blockchain.open_transactions)
+                print(self.blockchain.get_open_transactions())
             elif user_choice == 'm':
                 self.blockchain.mine_block()
             elif user_choice == 'p':
                 self.print_blockchain_elements()
             elif user_choice == 'c':
-                if verifier.verify_transactions(self.open_transactions, self.blockchain.get_balance):
+                if Verification.verify_transactions(self.blockchain.get_open_transactions(), self.blockchain.get_balance):
                     print('All transactions are valid')
                 else:
                     print('There are invalid transactions')
@@ -67,7 +66,7 @@ class Node:
                 waiting_for_input = False
             else:
                 print('Input was invalid, please pick a value from the list!')
-            if not verifier.verify_chain(self.blockchain.chain):
+            if not Verification.verify_chain(self.blockchain.get_chain()):
                 self.print_blockchain_elements()
                 print('Invalid blockchain!')
                 # Break out of the loop
--- verification.py
+++ verification.py
@@ -2,36 +2,8 @@ from hash_util import hash_string_256, hash_block
 
 
 class Verification:
-
-
-    def verify_chain(self, blockchain):
-        """ Verify the current blockchain and return True if it's valid, False otherwise."""
-        for (index, block) in enumerate(blockchain):
-            if index == 0:
-                continue
-            if block.previous_hash != hash_block(blockchain[index - 1]):
-                return False
-            if not self.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
-                print('Proof of work is invalid')
-                return False
-        return True
-
-
-    def verify_transaction(self, transaction, get_balance):
-        """Verify a transaction by checking whether the sender has sufficient coins.
-
-        Arguments:
-            :transaction: The transaction that should be verified.
-        """
-        sender_balance = get_balance()
-        return sender_balance >= transaction.amount
-
-    # This function accepts two arguments.
-    # One required one (transaction_amount) and one optional one (last_transaction)
-    # The optional one is optional because it has a default value => [1]
-
-
-    def valid_proof(self, transactions, last_hash, proof):
+    @staticmethod
+    def valid_proof(transactions, last_hash, proof):
         """Validate a proof of work number and see if it solves the puzzle algorithm (two leading 0s)
 
         Arguments:
@@ -51,6 +23,36 @@ class Verification:
         return guess_hash[0:2] == '00'
 
 
-    def verify_transactions(self, open_transactions, get_balance):
+
+    @classmethod
+    def verify_chain(cls, blockchain):
+        """ Verify the current blockchain and return True if it's valid, False otherwise."""
+        for (index, block) in enumerate(blockchain):
+            if index == 0:
+                continue
+            if block.previous_hash != hash_block(blockchain[index - 1]):
+                return False
+            if not cls.valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
+                print('Proof of work is invalid')
+                return False
+        return True
+
+    @staticmethod
+    def verify_transaction(transaction, get_balance):
+        """Verify a transaction by checking whether the sender has sufficient coins.
+
+        Arguments:
+            :transaction: The transaction that should be verified.
+        """
+        sender_balance = get_balance()
+        return sender_balance >= transaction.amount
+
+    # This function accepts two arguments.
+    # One required one (transaction_amount) and one optional one (last_transaction)
+    # The optional one is optional because it has a default value => [1]
+
+
+    @classmethod
+    def verify_transactions(cls, open_transactions, get_balance):
         """Verifies all open transactions."""
-        return all([self.verify_transaction(tx, get_balance) for tx in open_transactions])
+        return all([cls.verify_transaction(tx, get_balance) for tx in open_transactions])
